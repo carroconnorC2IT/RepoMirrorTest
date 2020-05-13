20ab8344edf371ecad28ed91a00fd10520a5ba4c
Carr O'Connor <carroconnor@c2itconsulting.net>
2020-05-13 10:29:34 -0400

    More Controls

---
+++ TelerikControls/TelerikControls/Views/DockLayout.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TelerikControls.Views
+{
+    public partial class DockLayout : ContentPage
+    {
+        public DockLayout()
+        {
+            InitializeComponent();
+        }
+    }
+}
